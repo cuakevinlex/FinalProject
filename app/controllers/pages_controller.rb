849e4e098f700af6324e8d71ce79c58c3a75e4c2
KevinCua <KevinCua@Zabrina-Cuas-MacBook.local>
2014-05-25 19:41:17 +0800

    ayos dit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,12 +4,16 @@ class PagesController < ApplicationController
   	@stalls = Stall.last(3)
   	@latest_reviews = Review.last(3) 
   	@stall = Stall.all.sample(1)
-  	while @food_of_the_day == nil
-		@sam = Food.all.sample(1)
-		if @sam[0].photo != ""
-			@food_of_the_day = @sam[0]
+  	@count = 0
+  	if Food.last != nil
+		while @food_of_the_day == nil and @count < 20
+			@sam = Food.all.sample(1)
+			if @sam[0].photo != ""
+				@food_of_the_day = @sam[0]
+			end
+			@count +=1
 		end
-  	end
+	end
   end
   def about
   	@cafeteria = Cafeterium.all
